44a2d8dd732766f286914de93ecb2cb08f460a3f
wangyang59 <wangyang59@baidu.com>
2018-09-13 17:10:02 -0700

    cleaned loss_utils.py

--- eval/evaluate_depth.py
+++ eval/evaluate_depth.py
@@ -55,15 +55,5 @@ def eval_depth(gt_depths,
                                     (disp_diff / gt_disp[mask]) >= 0.05)
         d1_all[i] = 100.0 * bad_pixels.sum() / mask.sum()
 
-    print("{:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}".
-          format('abs_rel', 'sq_rel', 'rms', 'log_rms', 'd1_all', 'a1', 'a2',
-                 'a3'))
-    print(
-        "{:10.4f}, {:10.4f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}".
-        format(abs_rel.mean(),
-               sq_rel.mean(),
-               rms.mean(),
-               log_rms.mean(), d1_all.mean(), a1.mean(), a2.mean(), a3.mean()))
-
     return abs_rel.mean(), sq_rel.mean(), rms.mean(), log_rms.mean(), a1.mean(
     ), a2.mean(), a3.mean(), d1_all.mean()
